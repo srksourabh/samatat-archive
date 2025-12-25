da09c80c1c7d93dbac129f7b4696bd40a382fcbb
srksourabh <srksourabh@gmail.com>
2025-12-25 19:13:28 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
   packages = [
     pkgs.nodejs_20
     pkgs.jdk17 # Java is often required for Firebase Emulators
+    pkgs.gh # GitHub CLI
   ];
   # Sets environment variables in the workspace
   env = {};
